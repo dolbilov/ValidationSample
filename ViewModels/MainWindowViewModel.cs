1cba5dc33db91646433275c00810a5dbb133396c
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-21 13:16:53 +0300

    chore: create VM with validation using exceptions inside setters

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,4 +3,6 @@
 public class MainWindowViewModel : ViewModelBase
 {
     public ValidationUsingDataAnnotationViewModel ValidationUsingDataAnnotationViewModel { get; } = new();
+
+    public ValidationUsingExceptionsInsideSetters ValidationUsingExceptionsInsideSetters { get; } = new();
 }
